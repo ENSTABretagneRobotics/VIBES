9d3218b5948dcae8cff9a468ddea2196ba1a09d3
Jeremy Nicola <jeremy.nicola@gmail.com>
2013-12-04 18:05:19 +0100

    Added minimalist client API for C++ + an "all_command" example.

---
+++ client-api/C++/src/vibes.cpp
@@ -0,0 +1,52 @@
+#include "vibes.h"
+#include <sstream>
+
+namespace vibes
+{    
+  FILE *channel;
+  /**
+  * Connects to the named pipe, not implemented yet.
+  */
+  void connect()
+  {
+    channel=fopen("vibes.json","w");
+  }
+  
+  void connect(std::string fileName)
+  {
+    channel=fopen("fileName","w");
+  }
+  
+  void disconnect()
+  {
+    fclose(channel);
+  }
+  
+  void figure()
+  {
+    std::string msg;
+    msg ="{\"action\":\"new\",\"figure\":\"default\"}\n\n";
+    fputs(msg.c_str(),channel);
+  }
+  
+  void figure(std::string figureName)
+  {
+    std::string msg;
+    msg ="{\"action\":\"new\",\"figure\":\""+figureName+"\"}\n\n";
+    fputs(msg.c_str(),channel);
+  }
+  
+  void drawBox(const double &x_lb, const double &x_ub, const double &y_lb, const double &y_ub)
+  {
+    std::stringstream msg;
+    msg<<"{\"action\":\"draw\",\"figure\":\"default\",\"shape\":{\"type\":\"box\",\"bounds\":["<<x_lb<<","<<x_ub<<","<<y_lb<<","<<y_ub<<"]}}\n\n";
+    fputs(msg.str().c_str(),channel);
+  }
+  
+  void drawBox(const double &x_lb, const double &x_ub, const double &y_lb, const double &y_ub, char color)
+  {
+    std::stringstream msg;
+    msg<<"{\"action\":\"draw\",\"figure\":\"default\",\"shape\":{\"type\":\"box\",\"color\":\""<<color<<"\",\"bounds\":["<<x_lb<<","<<x_ub<<","<<y_lb<<","<<y_ub<<"]}}\n\n";
+    fputs(msg.str().c_str(),channel);
+  }
+}
---
+++ client-api/C++/src/vibes.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string>
+
+namespace vibes
+{
+  //extern FILE *channel;  
+  
+  /**
+  * Connects to the named pipe, not implemented yet.
+  */
+  void connect();
+  void connect(std::string fileName);
+  
+  void disconnect();
+  
+  void figure();
+  void figure(std::string figureName);
+  
+  void drawBox(const double &x_lb, const double &x_ub, const double &y_lb, const double &y_ub);
+  void drawBox(const double &x_lb, const double &x_ub, const double &y_lb, const double &y_ub, char color);
+}
