e476828dde16dee7b3d55cc35722ceee1f9fc54a
Vincent Drevelle <vincent.drevelle@ensta-bretagne.fr>
2014-02-27 17:05:50 +0100

    Add a simple test of property list system to all_commands.cpp

--- client-api/C++/examples/all_commands.cpp
+++ client-api/C++/examples/all_commands.cpp
@@ -1,6 +1,13 @@
 #include "vibes.h"
 #include <iostream>
 
+namespace vibes {
+    int DrawTruc(int dummy, int dummy2, const Params &p = Params()) {
+        std::cout << p.toJSON() << '\n';
+        return p.size();
+    }
+}
+
 int main()
 {
   vibes::connect();
@@ -15,5 +22,6 @@ int main()
   std::cout << "drawBox with color" << std::endl;
   vibes::disconnect();
   std::cout << "disconnect" << std::endl;
+  std::cout << vibes::DrawTruc(666, 6666, VibesParams("LineWidth",5,"Parent",9.8897f,"Name","this is my name","Color",(vibes::RGB){14,45,255}));
   return 0;
 }
