1a3253280d2b452624427348f68d521f60523539
Vincent Drevelle <vincent.drevelle@ensta-bretagne.fr>
2014-06-23 14:20:02 +0200

    MATLAB API: Renamed axisLabel.m to axisLabels.m (to follow C++ API), and improved code of the function. Example drawing_3D.m updated accordingly.

--- client-api/MATLAB/src/+vibes/axisLabel.m
+++
@@ -1,7 +0,0 @@
-function axisLabel( labels )
-global vibesPipeFile;
-global current_fig;
-    msg=struct('action','set','figure',current_fig,'properties',struct('axislabels',labels));
-    fprintf(vibesPipeFile,'%s\n\n',vibes.json.dump(msg));
-end
-
