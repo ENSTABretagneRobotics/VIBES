e5654ec2c67610c29b9b8f703337f03e2eb69ee7
Vincent Drevelle <vincent.drevelle@irisa.fr>
2015-02-09 16:24:15 +0100

    Add a dialog for editing named objects (double-click on the tree view). - Only basic colour editing is provided for now. - Tree model has been modified, but is still messy. A very dirty trick has been used to ensure actual graphics / model syncronization, but it should be reworked ASAP (the full model is updated everytime a message is processed)

---
+++ viewer/propertyeditdialog.cpp
@@ -0,0 +1,37 @@
+#include "propertyeditdialog.h"
+#include "ui_propertyeditdialog.h"
+
+#include <QDebug>
+
+PropertyEditDialog::PropertyEditDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::PropertyEditDialog)
+{
+    ui->setupUi(this);
+}
+
+PropertyEditDialog::~PropertyEditDialog()
+{
+    delete ui;
+}
+
+QJsonObject PropertyEditDialog::showEditorForJson(QJsonObject init)
+{
+    PropertyEditDialog *dlg = new PropertyEditDialog();
+    dlg->ui->comboBox_LineStyle->setCurrentText(init["LineStyle"].toString());
+    dlg->ui->comboBox_FaceColor->setCurrentText(init["FaceColor"].toString());
+    dlg->ui->comboBox_EdgeColor->setCurrentText(init["EdgeColor"].toString());
+
+    QJsonObject json;
+
+    if (dlg->exec() == Accepted)
+    {
+        json["LineStyle"] = QJsonValue(dlg->ui->comboBox_LineStyle->currentText());
+        json["FaceColor"] = QJsonValue(dlg->ui->comboBox_FaceColor->currentText());
+        json["EdgeColor"] = QJsonValue(dlg->ui->comboBox_EdgeColor->currentText());
+    }
+
+    qDebug() << "dlg json result" << json;
+
+    return json;
+}
