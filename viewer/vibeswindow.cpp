ENSTABretagneRobotics/VIBES

commit e5654ec2c67610c29b9b8f703337f03e2eb69ee7

Add a dialog for editing named objects (double-click on the tree view). - Only basic colour editing is provided for now. - Tree model has been modified, but is still messy. A very dirty trick has been used to ensure actual graphics / model syncronization, but it should be reworked ASAP (the full model is updated everytime a message is processed)

viewer/vibeswindow.cpp
@@ -16,6 +16,9 @@
 
 #include "vibestreemodel.h"
 
+#include "propertyeditdialog.h"
+#include <QJsonObject>
+
 VibesWindow::VibesWindow(bool showFileOpenDlg, QWidget *parent) :
 QMainWindow(parent),
 ui(new Ui::VibesWindow)
@@ -25,10 +28,21 @@ ui(new Ui::VibesWindow)
 
     // When its name is double clicked in the list, the corresponding figure is brought to front
     connect(ui->treeView, &QTreeView::doubleClicked,
-            [](const QModelIndex& mi){Figure2D* fig = static_cast<Figure2D*>( mi.internalPointer() );
+            [this](const QModelIndex& mi){//Figure2D* fig = static_cast<Figure2D*>( mi.internalPointer() );
+                                      Figure2D* fig = 0;
+                                      if (mi.internalPointer() == 0) { fig = this->figures.values().at(mi.row()); }
                                       if (fig) { fig->showNormal();
                                                  fig->activateWindow();
                                                  fig->raise(); }
+                                     } );
+    // When a object name is double clicked in the list, the property editor is shown
+    connect(ui->treeView, &QTreeView::doubleClicked,
+            [](const QModelIndex& mi){Figure2D* fig = static_cast<Figure2D*>( mi.internalPointer() );
+                                      if (fig) { VibesScene2D * scene = fig->scene();
+                                                 VibesGraphicsItem * item = scene->itemByName(scene->namedItems().at(mi.row()));
+                                                 QJsonObject json = PropertyEditDialog::showEditorForJson(item->json());
+                                                 item->setJsonValues(json);
+                                               }
                                      } );
 
     /// \todo Put platform dependent code here for named pipe creation and opening
@@ -312,6 +326,10 @@ VibesWindow::processMessage(const QByteArray &msg_data)
     {
         return false;
     }
+
+    /// \todo Do not force update after each message
+    static_cast<VibesTreeModel*> (ui->treeView->model())->forceUpdate();
+
     return true;
 }
 

viewer/CMakeLists.txt
@@ -42,10 +42,11 @@ SET(VIBes_viewer_SOURCES main.cpp
                          vibesscene2d.cpp
                          figure2d.cpp
                          vibestreemodel.cpp
-                         vibeswindow.cpp )
+                         vibeswindow.cpp
+                         propertyeditdialog.cpp )
 
 # Qt designer UI files
-SET(VIBes_viewer_FORMS vibeswindow.ui)
+SET(VIBes_viewer_FORMS vibeswindow.ui propertyeditdialog.ui)
 # Process Qt UI files
 QT5_WRAP_UI(VIBes_viewer_FORMS_HEADERS ${VIBes_viewer_FORMS})
 

viewer/propertyeditdialog.cpp
@@ -1,0 +1,37 @@
+#include "propertyeditdialog.h"
+#include "ui_propertyeditdialog.h"
+
+#include <QDebug>
+
+PropertyEditDialog::PropertyEditDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::PropertyEditDialog)
+{
+    ui->setupUi(this);
+}
+
+PropertyEditDialog::~PropertyEditDialog()
+{
+    delete ui;
+}
+
+QJsonObject PropertyEditDialog::showEditorForJson(QJsonObject init)
+{
+    PropertyEditDialog *dlg = new PropertyEditDialog();
+    dlg->ui->comboBox_LineStyle->setCurrentText(init["LineStyle"].toString());
+    dlg->ui->comboBox_FaceColor->setCurrentText(init["FaceColor"].toString());
+    dlg->ui->comboBox_EdgeColor->setCurrentText(init["EdgeColor"].toString());
+
+    QJsonObject json;
+
+    if (dlg->exec() == Accepted)
+    {
+        json["LineStyle"] = QJsonValue(dlg->ui->comboBox_LineStyle->currentText());
+        json["FaceColor"] = QJsonValue(dlg->ui->comboBox_FaceColor->currentText());
+        json["EdgeColor"] = QJsonValue(dlg->ui->comboBox_EdgeColor->currentText());
+    }
+
+    qDebug() << "dlg json result" << json;
+
+    return json;
+}

viewer/propertyeditdialog.h
@@ -1,0 +1,26 @@
+#ifndef PROPERTYEDITDIALOG_H
+#define PROPERTYEDITDIALOG_H
+
+#include <QDialog>
+#include <QJsonObject>
+
+namespace Ui {
+class PropertyEditDialog;
+}
+
+class PropertyEditDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit PropertyEditDialog(QWidget *parent = 0);
+    ~PropertyEditDialog();
+
+private:
+    Ui::PropertyEditDialog *ui;
+
+public:
+    static QJsonObject showEditorForJson(QJsonObject init);
+};
+
+#endif // PROPERTYEDITDIALOG_H

viewer/propertyeditdialog.ui
@@ -1,0 +1,328 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<ui version="4.0">
+ <class>PropertyEditDialog</class>
+ <widget class="QDialog" name="PropertyEditDialog">
+  <property name="geometry">
+   <rect>
+    <x>0</x>
+    <y>0</y>
+    <width>240</width>
+    <height>139</height>
+   </rect>
+  </property>
+  <property name="windowTitle">
+   <string>Dialog</string>
+  </property>
+  <widget class="QDialogButtonBox" name="buttonBox">
+   <property name="geometry">
+    <rect>
+     <x>10</x>
+     <y>100</y>
+     <width>221</width>
+     <height>41</height>
+    </rect>
+   </property>
+   <property name="orientation">
+    <enum>Qt::Horizontal</enum>
+   </property>
+   <property name="standardButtons">
+    <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
+   </property>
+  </widget>
+  <widget class="QWidget" name="formLayoutWidget">
+   <property name="geometry">
+    <rect>
+     <x>10</x>
+     <y>10</y>
+     <width>221</width>
+     <height>91</height>
+    </rect>
+   </property>
+   <layout class="QFormLayout" name="formLayout">
+    <item row="0" column="0">
+     <widget class="QLabel" name="label">
+      <property name="text">
+       <string>Edge color</string>
+      </property>
+     </widget>
+    </item>
+    <item row="1" column="0">
+     <widget class="QLabel" name="label_2">
+      <property name="text">
+       <string>Face color</string>
+      </property>
+     </widget>
+    </item>
+    <item row="1" column="1">
+     <widget class="QComboBox" name="comboBox_FaceColor">
+      <item>
+       <property name="text">
+        <string/>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>transparent</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>blue</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>cyan</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>green</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>yellow</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>red</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>magenta</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>white</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>lightGray</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>gray</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkGray</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>black</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkBlue</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkCyan</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkGreen</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkYellow</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkRed</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkMagenta</string>
+       </property>
+      </item>
+     </widget>
+    </item>
+    <item row="0" column="1">
+     <widget class="QComboBox" name="comboBox_EdgeColor">
+      <item>
+       <property name="text">
+        <string/>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>transparent</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>blue</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>cyan</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>green</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>yellow</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>red</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>magenta</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>white</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>lightGray</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>gray</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkGray</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>black</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkBlue</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkCyan</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkGreen</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkYellow</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkRed</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>darkMagenta</string>
+       </property>
+      </item>
+     </widget>
+    </item>
+    <item row="2" column="0">
+     <widget class="QLabel" name="label_3">
+      <property name="text">
+       <string>Line style</string>
+      </property>
+     </widget>
+    </item>
+    <item row="2" column="1">
+     <widget class="QComboBox" name="comboBox_LineStyle">
+      <item>
+       <property name="text">
+        <string/>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>--</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>-.</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>-</string>
+       </property>
+      </item>
+      <item>
+       <property name="text">
+        <string>:</string>
+       </property>
+      </item>
+     </widget>
+    </item>
+   </layout>
+  </widget>
+ </widget>
+ <resources/>
+ <connections>
+  <connection>
+   <sender>buttonBox</sender>
+   <signal>accepted()</signal>
+   <receiver>PropertyEditDialog</receiver>
+   <slot>accept()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>248</x>
+     <y>254</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>157</x>
+     <y>274</y>
+    </hint>
+   </hints>
+  </connection>
+  <connection>
+   <sender>buttonBox</sender>
+   <signal>rejected()</signal>
+   <receiver>PropertyEditDialog</receiver>
+   <slot>reject()</slot>
+   <hints>
+    <hint type="sourcelabel">
+     <x>316</x>
+     <y>260</y>
+    </hint>
+    <hint type="destinationlabel">
+     <x>286</x>
+     <y>274</y>
+    </hint>
+   </hints>
+  </connection>
+ </connections>
+</ui>

viewer/vibesscene2d.h
@@ -22,6 +22,7 @@ public:
     void addVibesItem(VibesGraphicsItem *item);
     VibesGraphicsItem * itemByName(QString name) {if (_namedItems.contains(name)) return _namedItems[name]; else return 0;}
     void setItemName(VibesGraphicsItem *item, QString name);
+    QList<QString> namedItems() const {return _namedItems.keys();}
 
     const int nbDim() const { return _nbDim; }
     const int dimX() const { return _dimX; }

viewer/vibestreemodel.cpp
@@ -12,7 +12,7 @@ QModelIndex VibesTreeModel::index(int row, int column, const QModelIndex &parent
     if (!hasIndex(row, column, parent))
         return QModelIndex();
 
-    const void *parentItem;
+ /*   const void *parentItem;
 
     if (!parent.isValid())
         parentItem = figures;
@@ -28,7 +28,22 @@ QModelIndex VibesTreeModel::index(int row, int column, const QModelIndex &parent
             return createIndex(row, column, figures->values().at(row));
         else
             return QModelIndex();
+    }*/
+
+    if (!parent.isValid())
+    {
+        if (row < figures->size())
+            return createIndex(row, column, (void*)0);
+        else
+            return QModelIndex();
     }
+
+    if (parent.internalPointer() == 0)
+    {
+        return createIndex(row, column, figures->values().at(parent.row()));
+    }
+
+
 
     return QModelIndex();
 }
@@ -38,17 +53,25 @@ QModelIndex VibesTreeModel::parent(const QModelIndex &index) const
     if (!index.isValid())
         return QModelIndex();
 
+    // Parent of a figure is root
+    // figures have 0 as internal ptr
     const void *childItem = index.internalPointer();
-    if (childItem == figures)
+    if (childItem == 0)
     {
         return QModelIndex();
     }
 
-    if (!figures->key(static_cast<Figure2D*>(index.internalPointer())).isEmpty())
+/*    if (!figures->key(static_cast<Figure2D*>(index.internalPointer())).isEmpty())
     {
-        return QModelIndex();
+        return QModelIndex(0, 0, 0);
     }
-
+    */
+    QList<Figure2D*> figPtrs = figures->values();
+    int row = figPtrs.indexOf(static_cast<Figure2D*>(index.internalPointer()));
+    if (row >= 0)
+    {
+        return createIndex(row, 0, (void*)0);
+    }
 
     return QModelIndex();
 }
@@ -57,7 +80,7 @@ int VibesTreeModel::rowCount(const QModelIndex &parent) const
 {
     if (parent.column() > 0)
         return 0;
-
+/*
     const void *parentItem;
     if (!parent.isValid())
         parentItem = figures;
@@ -68,6 +91,20 @@ int VibesTreeModel::rowCount(const QModelIndex &parent) const
     {
         return figures->size();
     }
+    */
+
+    // root node
+    if (!parent.isValid())
+    {
+        return figures->size();
+    }
+    // figure node
+    if (parent.internalPointer() == 0)
+    {
+        Figure2D * fig = figures->values().at(parent.row());
+        return fig->scene()->namedItems().size();
+    }
+
     return 0;
 }
 
@@ -87,9 +124,9 @@ QVariant VibesTreeModel::data(const QModelIndex &index, int role) const
     if (role == Qt::DecorationRole)
     {
         const void *childItem = index.internalPointer();
-        if (childItem == figures)
+        if (childItem == 0)
         {
-            return QVariant();
+        //    return QColor(Qt::green);
         }
         if (const Figure2D* childFigure = static_cast<const Figure2D*>(childItem))
         {
@@ -102,13 +139,18 @@ QVariant VibesTreeModel::data(const QModelIndex &index, int role) const
         return QVariant();
 
     const void *childItem = index.internalPointer();
-    if (childItem == figures)
+    if (childItem == 0)
     {
-        return QVariant("Top level");
+        //return QVariant("Top level");
+        //return figures->key(const_cast<Figure2D*>(childFigure), "Unnamed figure");
+        Figure2D* fig = figures->values().at(index.row());
+        return figures->key(fig, "Unnamed figure");
     }
     if (const Figure2D* childFigure = static_cast<const Figure2D*>(childItem))
     {
-        return figures->key(const_cast<Figure2D*>(childFigure), "Unnamed figure");
+        //return figures->key(const_cast<Figure2D*>(childFigure), "Unnamed figure");
+        //return QString("id=%1 row=%2").arg(index.internalId()).arg(index.row());
+        return childFigure->scene()->namedItems().at(index.row());
     }
 
     return QVariant();

viewer/vibeswindow.ui
@@ -18,7 +18,7 @@
     <item>
      <widget class="QTreeView" name="treeView">
       <property name="rootIsDecorated">
-       <bool>false</bool>
+       <bool>true</bool>
       </property>
      </widget>
     </item>
@@ -30,7 +30,7 @@
      <x>0</x>
      <y>0</y>
      <width>166</width>
-     <height>21</height>
+     <height>22</height>
     </rect>
    </property>
   </widget>

commit 8720a324843bcd29f3377e10e02fde88a6ec4877

Restores graphics export button functionality (regression introduced in e5654ec2c67610c29b9b8f703337f03e2eb69ee7)

viewer/vibeswindow.cpp
@@ -351,7 +351,12 @@ void VibesWindow::exportCurrentFigureGraphics()
     if (!selectId.isValid())
         return;
     // If the selected item is a figure, export it
-    Figure2D * pfig = static_cast<Figure2D*> (selectId.internalPointer());
+    Figure2D * pfig = 0;
+    if (!selectId.internalPointer())
+        pfig = figures.values().at(selectId.row());
+    else
+        pfig = static_cast<Figure2D*> (selectId.internalPointer());
+    
     if (figures.values().contains(pfig))
     {
         pfig->exportGraphics();